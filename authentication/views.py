9c576edfa70f6e77fc701ded102f08062247ccfb
Nikolay Berchev <ber4ev@abv.bg>
2020-12-23 23:35:29 +0200

    Added authentication system

--- authentication/views.py
+++ authentication/views.py
@@ -1,12 +1,21 @@
 import json
 
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
 from django.views import View
 from django.http import JsonResponse
 from django.contrib.auth.models import User
 from django.contrib import messages
+from django.contrib import auth
+
+from django.core.mail import EmailMessage
+from django.utils.encoding import force_bytes, force_text, DjangoUnicodeDecodeError
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.contrib.sites.shortcuts import get_current_site
 
 from validate_email import validate_email
+
+from .utils import token_generator
 
 
 class UsernameValidationView(View):
@@ -42,4 +51,93 @@ class RegistrationView(View):
         email = request.POST['email']
         password = request.POST['password']
 
+        context = {
+            'fieldValues': request.POST,
+        }
+
+        if not User.objects.filter(username=username).exists():
+            if not User.objects.filter(email=email).exists():
+                if len(password) < 6:
+                    messages.error(request, 'Password too short')
+                    return render(request, 'authentication/register.html', context)
+
+                user = User.objects.create_user(username=username, email=email)
+                user.set_password(password)
+                user.is_active = False
+                user.save()
+
+                domain = get_current_site(request).domain
+                uidb64 = urlsafe_base64_encode(force_bytes(user.pk))
+                link = reverse('activate', kwargs={'uidb64': uidb64,
+                                                   'token': token_generator.make_token(user)})
+                activate_url = 'http://'+domain+link
+
+                email_subject = 'Activate your account'
+                email_body = f'Hi {user.username}! Please use this link to verify your account:\n{activate_url}'
+
+                email_to_send = EmailMessage(
+                    email_subject,
+                    email_body,
+                    'noreply@nberchev.com',
+                    [email],
+                )
+
+                email_to_send.send(fail_silently=False)
+
+                messages.success(request, 'Account successfully created')
+                return render(request, 'authentication/register.html')
+
         return render(request, 'authentication/register.html')
+
+
+class LoginView(View):
+    def get(self, request):
+        return render(request, 'authentication/login.html')
+
+    def post(self, request):
+        username = request.POST['username']
+        password = request.POST['password']
+
+        if username and password:
+            user = auth.authenticate(username=username, password=password)
+
+            if user:
+                if user.is_active:
+                    auth.login(request, user)
+                    messages.success(request, f'Welcome, {user.username}! You are now logged in!')
+                    return redirect('expenses')
+                messages.error(request, 'Account is not active, please check your email')
+                return render(request, 'authentication/login.html')
+            messages.error(request, 'Invalid credentials, try again')
+            return render(request, 'authentication/login.html')
+        messages.error(request, 'Please fill all fields')
+        return render(request, 'authentication/login.html')
+
+
+class LogoutView(View):
+    def post(self, request):
+        auth.logout(request)
+        messages.success(request, 'You have been signed out')
+        return redirect('login')
+
+
+class VerificationView(View):
+    def get(self, request, uidb64, token):
+        try:
+            id = force_text(urlsafe_base64_decode(uidb64))
+            user = User.objects.get(pk=id)
+
+            if not token_generator.check_token(user, token):
+                return redirect('login'+'?message='+'User already activated')
+
+            if user.is_active:
+                return redirect('login')
+            user.is_active = True
+            user.save()
+
+            messages.success(request, 'Account activated successfully')
+            return redirect('login')
+        except Exception as ex:
+            pass
+
+        return redirect('login')
