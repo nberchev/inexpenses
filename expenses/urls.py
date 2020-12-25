26590060ee261fe13d3add6c779b001f64508d1c
Nikolay Berchev <ber4ev@abv.bg>
2020-12-25 23:09:21 +0200

    Full CRUD and pagination for expenses

--- expenses/urls.py
+++ expenses/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from .views import index, add_expense
+from .views import index, add_expense, edit_expense, delete_expense
 
 
 urlpatterns = [
     path('', index, name='expenses'),
     path('add-expense/', add_expense, name='add-expense'),
+    path('edit-expense/<int:pk>/', edit_expense, name='edit-expense'),
+    path('delete-expense/<int:pk>/', delete_expense, name='delete-expense'),
 ]
--- expenses/views.py
+++ expenses/views.py
@@ -1,11 +1,94 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator
+
+from .models import Category, Expense
 
 
 @login_required(login_url='/authentication/login')
 def index(request):
-    return render(request, 'expenses/index.html')
+    categories = Category.objects.all()
+    expenses = Expense.objects.filter(owner=request.user)
+    paginator = Paginator(expenses, 2)
+    page_number = request.GET.get('page')
+    page_obj = Paginator.get_page(paginator, page_number)
+
+    context = {
+        'expenses': expenses,
+        'page_obj': page_obj,
+    }
+    return render(request, 'expenses/index.html', context)
 
 
 def add_expense(request):
-    return render(request, 'expenses/add_expense.html')
+    categories = Category.objects.all()
+    context = {
+        'categories': categories,
+        'values': request.POST,
+    }
+    if request.method == 'GET':
+        return render(request, 'expenses/add_expense.html', context)
+
+    else:
+        amount = request.POST['amount']
+
+        if not amount:
+            messages.error(request, 'Amount field is required')
+            return render(request, 'expenses/add_expense.html', context)
+
+        description = request.POST['description']
+        date = request.POST['expense_date']
+        category = request.POST['category']
+
+        if not description:
+            messages.error(request, 'Description field is required')
+            return render(request, 'expenses/add_expense.html', context)
+
+        Expense.objects.create(owner=request.user, amount=amount, date=date, category=category, description=description)
+        messages.success(request, 'Expense saved successfully')
+        return redirect('expenses')
+
+
+def edit_expense(request, pk):
+    expense = Expense.objects.get(pk=pk)
+    categories = Category.objects.all()
+    context = {
+        'expense': expense,
+        'values': expense,
+        'categories': categories,
+    }
+
+    if request.method == 'GET':
+        return render(request, 'expenses/edit_expense.html', context)
+    else:
+        amount = request.POST['amount']
+
+        if not amount:
+            messages.error(request, 'Amount field is required')
+            return render(request, 'expenses/edit_expense.html', context)
+
+        description = request.POST['description']
+        date = request.POST['expense_date']
+        category = request.POST['category']
+
+        if not description:
+            messages.error(request, 'Description field is required')
+            return render(request, 'expenses/edit_expense.html', context)
+
+        expense.owner = request.user
+        expense.amount = amount
+        expense.date = date
+        expense.category = category
+        expense.description = description
+        expense.save()
+
+        messages.success(request, 'Expense updated successfully')
+        return redirect('expenses')
+
+
+def delete_expense(request, pk):
+    expense = Expense.objects.get(pk=pk)
+    expense.delete()
+    messages.success(request, 'Expense deleted successfully')
+    return redirect('expenses')
