9bb5c7a0960043951eb99dd2b9e8d80a9717fee1
Nikolay Berchev <ber4ev@abv.bg>
2020-12-28 23:12:58 +0200

    Added export to csv and to xls functionalities

--- expenses/urls.py
+++ expenses/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from django.views.decorators.csrf import csrf_exempt
 
-from .views import index, add_expense, edit_expense, delete_expense, search_expenses, expense_category_summary, stats_view
+from .views import index, add_expense, edit_expense, delete_expense, search_expenses, expense_category_summary, \
+    stats_view, export_csv, export_excel
 
 
 urlpatterns = [
@@ -12,4 +13,6 @@ urlpatterns = [
     path('search-expenses/', csrf_exempt(search_expenses), name='search_expenses'),
     path('expense_category_summary/', expense_category_summary, name='expense_category_summary'),
     path('stats/', stats_view, name='stats'),
+    path('export-csv/', export_csv, name='export-csv'),
+    path('export-excel/', export_excel, name='export-excel'),
 ]
--- expenses/views.py
+++ expenses/views.py
@@ -1,11 +1,13 @@
 import json
 import datetime
+import csv
+import xlwt
 
 from django.contrib import messages
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
 
 from userpreferences.models import UserPreference
 
@@ -144,3 +146,48 @@ def expense_category_summary(request):
 
 def stats_view(request):
     return render(request, 'expenses/stats.html')
+
+
+def export_csv(request):
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachment; filename=Expenses'+str(datetime.datetime.now())+'.csv'
+
+    writer = csv.writer(response)
+    writer.writerow(['Amount', 'Description', 'Category', 'Date'])
+
+    expenses = Expense.objects.filter(owner=request.user)
+
+    for expense in expenses:
+        writer.writerow([expense.amount, expense.description, expense.category, expense.date])
+
+    return response
+
+
+def export_excel(request):
+    response = HttpResponse(content_type='application/ms-excel')
+    response['Content-Disposition'] = 'attachment; filename=Expenses' + str(datetime.datetime.now()) + '.xls'
+
+    wb = xlwt.Workbook(encoding='utf-8')
+    ws = wb.add_sheet('Expenses')
+    row_num = 0
+    font_style = xlwt.XFStyle()
+    font_style.font.bold = True
+
+    columns = ['Amount', 'Description', 'Category', 'Date']
+
+    for col_num in range(len(columns)):
+        ws.write(row_num, col_num, columns[col_num], font_style)
+
+    font_style = xlwt.XFStyle()
+
+    rows = Expense.objects.filter(owner=request.user).values_list('amount', 'description', 'category', 'date')
+
+    for row in rows:
+        row_num += 1
+
+        for col_num in range(len(row)):
+            ws.write(row_num, col_num, str(row[col_num]), font_style)
+
+    wb.save(response)
+
+    return response
